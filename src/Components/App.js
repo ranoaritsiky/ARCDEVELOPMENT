0f4f0fcc9ace9ab153f83b6e4c1c350d7ae8bea0
Tsiky <tsiky@test.com>
2022-07-15 17:41:17 +0300

    add styling

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,23 +1,44 @@
-import Header from './ui/header'
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
+import React from "react";
+// import {ThemeProvider} from '@mui/styles';
+
+import Header from "./ui/header";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+
+import { testCenter,useStyles,styleHr } from "./ui/theme";
+
+
+function StylingButton(){
+  const classes=useStyles()
+  return <Button className={classes.root}>Styled with hook MF</Button>
+}
+
 function App() {
+  const classes = testCenter();
+  const classesHr=styleHr()
   return (
-    <div className="App">
-      <Header/>
+    <>
+      <Header />
       <Container>
         <Box sx={{ my: 2 }}>
-          {[...new Array(120)]
+          {[...new Array(12)]
             .map(
-              () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
+              () => `
+              Cras mattis consectetur purus sit amet fermentum.
+              Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+              Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
             )
-            .join('\n')}
+            .join("\n")}
         </Box>
+        <div className={classes.root}>
+
+          <StylingButton />
+        </div>
       </Container>
-    </div>
+      <hr className={classesHr.root}/>
+    </>
   );
 }
 
