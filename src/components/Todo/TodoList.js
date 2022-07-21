4f00f4d0e41329fb8767c0cfa4f9ba0d1dfb00fa
Tsiky <tsiky@test.com>
2022-07-21 11:50:52 +0300

    change todo list for delete

--- src/components/Todo/TodoList.js
+++ src/components/Todo/TodoList.js
@@ -1,23 +1,18 @@
 export default function TodoList(props) {
+    
     return (
         <div className="d-flex flex-row m-2 w-20">
-            {
-                props.todoList.map((todo, index) => (
-                    <div key={index} className="card m-2" style={{"width": "18rem"}}>
-                        <div className="card-body">
-                            <h5 className="card-title">{index + 1}-{todo.todo}</h5>
-                            <h6 className="card-subtitle mb-2 text-muted">Description</h6>
-                            <p className="card-text">
-                                {todo.description}
-                            </p>
-                            <button className="btn btn-primary">Done</button>
-                            <button className="btn btn-secondary m-2">Delete</button>
-                            <button className="btn btn-danger">Cancel</button>
-                        </div>
-                    </div>
-                    
-                ))
-            }
+            <div  className="card m-2" style={{"width": "18rem"}}>
+                <div className="card-body">
+                    <h5 className="card-title">{props.count} - {props.name}</h5>
+                    <h6 className="card-subtitle mb-2 text-muted">Description</h6>
+                    <p className="card-text">
+                        {props.description}
+                    </p>
+                    <button className="btn btn-primary" onClick={()=>props.changeStatus(props.id)}>Done</button>
+                    <button className="btn btn-secondary m-2" onClick={()=>props.deleteTodo(props.id)}>Delete</button>
+                </div>
+            </div>
         </div>
     )
 }
