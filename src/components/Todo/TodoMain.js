931e722b5de8271ff494ea310be4d20f7715bd6b
Tsiky <tsiky@test.com>
2022-07-21 11:51:45 +0300

    update todomain with mapping todolist

--- src/components/Todo/TodoMain.js
+++ src/components/Todo/TodoMain.js
@@ -1,19 +1,40 @@
-import {useState} from 'react' 
-import TodoForm from './TodoForm'
-import TodoList from './TodoList'
+import { useState } from "react";
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
 
+export default function TodoMain() {
+  const [todo, setTodo] = useState([]);
 
-export default function TodoMain(){
-    const [todo, setTodo] = useState([])
+  const addTodo = (val) => {
+    setTodo([...todo, val]);
+  };
+
+  const deleteTodo = (val) => {
+    setTodo(todo.filter((todo) => todo.id !== val));
+  };
+
+  const changeStatus=(val)=>{
+    let value=todo.filter((todo) => todo.id === val)
+        
+  }
+
+  return (
+    <div className="d-flex flex-row">
     
-    const addTodo = (val) => {
-        setTodo([...todo,val])
-    }
-
-    return (
-        <div className="d-flex flex-row">
-            <TodoForm addTodo={addTodo}/>
-            <TodoList todoList={todo}/>
-        </div>
-    )
+      <TodoForm addTodo={addTodo} />
+      {/* show list of todo in card */}
+      {todo.map((todo, index) => (
+        <TodoList
+          key={index}
+          count={index + 1}
+          name={todo.todo}
+          description={todo.description}
+          id={todo.id}
+          deleteTodo={deleteTodo}
+          changeStatus={changeStatus}
+          status={todo.status}
+        />
+      ))}
+    </div>
+  );
 }
