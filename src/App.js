e06caac09470aaae180bd1b5f9c519abe0a8b532
Tsiky <tsiky@test.com>
2022-07-21 10:06:53 +0300

    add counter in App to use counter using reducer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import NavBar from './components/Header/Header'
 import TodoMain from './components/Todo/TodoMain'
+import Counter from './components/Count/Index.js'
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <TodoMain/>
+      <Counter/>
     </div>
   );
 }
