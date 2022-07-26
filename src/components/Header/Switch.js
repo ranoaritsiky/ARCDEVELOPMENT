a0396037126ff0393df41bba87cc2542e8afb3d5
Tsiky <tsiky@test.com>
2022-07-26 10:56:13 +0300

    change theme by props

--- src/components/Header/Switch.js
+++ src/components/Header/Switch.js
@@ -1,19 +1,14 @@
-
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import Switch from "@mui/material/Switch";
 import { themeContext } from "../../contexts/themeContext";
 
 const label = { inputProps: { "aria-label": "Switch demo" } };
 
-export default function BasicSwitches() {
-  const [switchState, setSwitchState] = useState(false);
+export default function BasicSwitches(props) {
   const { setState } = useContext(themeContext);
-  
-
   const toogleSwitch = () => {
-    setSwitchState(!switchState)
-    setState(switchState)
+    setState(!props.stateTheme);
   };
 
-  return <Switch {...label} defaultChecked  color="secondary" onChange={toogleSwitch} />;
+  return <Switch {...label} color="secondary" onChange={toogleSwitch} />;
 }
